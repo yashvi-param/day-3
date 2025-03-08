c8959c1fa5df5f574b4a9ca8cd436124f43fec06
yashvi-param <yashvirwn123@gmail.com>
2025-03-08 14:05:10 +0530

    Add files via upload
    uncomplete

---
+++ index.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //arithmatic operators
+    // int a;
+    // int b; 
+    // cin >> a;
+    // cin >> b;
+
+    // cout << "addition of a & b =  " << a+b << endl;
+    // cout << "substraction of a & b = " << a-b << endl;
+    // cout << "devidation of a & b = " << a/b << endl;
+    // cout << "multiplication of a & b = " << a*b << endl;
+    // cout << "moduls of a & b ="<< a%b << endl;
+
+//relational operators 
+
+    // int x=10;
+    // int y=20;
+    // int z=5;
+
+  // x < y  true
+ // x > y  false
+ // x == y  false
+ // x <= y  true
+ // x >= y false
+ // x != y true
+
+ //logical operators
+
+ //  10 == 20 && 20 == 20; false
+//  10 == 20 || 20 == 20; true
+//  10 == 20 || 20 == 30; true
+
+//  x < y && y < z
+//  x < y && y < z
+
+//Assignment operators 
+ 
+int x=10;
+     int y=20;
+   int z=5;
+
+
+    z=x+y;
+
+    int a = 100;
+    int b = 20;
+
+    b=a+b;
+    b+=a;
+
+
+}
+ 
+
+ 
---
+++ index2.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int a; 
+    // int b;
+    // cin >> a;
+    // cin >> b;
+
+    // cout << "addtion of a & b = " << a+b << endl;
+    // cout << "substraction of a & b = " << a-b << endl;
+    // cout << "devidation of a & b = " << a/b << endl;
+    // cout << "multiplication of a & b = " << a*b << endl;
+    // cout << "modulus of a & b = " << a%b << endl;    
+
+
+
+    // int x = 10;
+    // int y = 20;
+    // int z = 5;
+
+  //  relational operators
+
+ // x < y  true
+ // x > y  false
+ // x == y  false
+ // x <= y  true
+ // x >= y false
+ // x != y true
+
+
+ // logical operators
+
+
+//  10 == 20 && 20 == 20; false
+//  10 == 20 || 20 == 20; true
+//  10 == 20 || 20 == 30; true
+
+//  x < y && y < z
+//  x < y && y < z
+
+
+// assignment operators
+
+
+// z = x+y; // 30
+
+// int a = 100;
+// int b = 50;
+
+// // b = a+b;  // 
+// b+=a;
+
+// int num = 100000000000000;
+
+
+// cout << sizeof(b) << endl;
+// cout << sizeof(num) << endl;
+
+
+int n = 20;
+int n2 = 20;
+n++; // 21
+n++;  // 22
+n++;  // 23
+
+--n2; // 19
+--n2; // 18
+n2--; // 17
+
+// int num3 = 5;
+// int num4 = 5;
+
+// cout << --num3 << endl;
+// cout << num4-- << endl;
+// cout << --num4 << endl;
+
+// cout << n << " " << n2 << endl;
+
+cout <<  1 + 4 * 2 - 3 / 3 + 2 * 2 / 4 << endl;
+
+
+// 1 + 4 * 2 - 1 + 2 * 2
+// 1 + 8 - 1 + 4
+// 8 + 4
+//12
+
+
+
+
+
+
+
+ // 1 + 8 -3 / 3 +2 * 2
+ // 1 + 8 - 3 / 3 + 4
+ // 1 + 8 - 1 + 4
+ // 1 + 8 - 5
+ // 1 + 3
+ // 4
+
+    return 0;
+}
